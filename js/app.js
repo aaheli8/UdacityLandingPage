528ec01a05d9a4120c32ee17364078c617a86bf5
aaheli8 <sadhukhanaheli@gmail.com>
2023-06-20 13:56:01 +0530

    Changes in Javascript

--- js/app.js
+++ js/app.js
@@ -1,62 +1,65 @@
 /**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
  * Define Global Variables
  * 
 */
+const navlist=document.getElementById('navbar__list');
 
+// build the nav bar dynamically
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+const navBar=[
+    {label:'Home', url:'#home'},
+    {label:'About',url: '#about' },
+    {label:'Services',url: '#services' },
+    {label:'Contact' , url:'#contact'}
+];
+ 
+navBar.forEach(item => {
+    const list=document.createElement('li');
+    const link=document.createElement('a');
+    link.href=item.url;
+    link.textContent=item.label;
 
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
+    list.appendChild(link);
+    navlist.appendChild(list);
+});
 
 // Add class 'active' to section when near top of viewport
+// This will highlight which section is being viewed
+// Used the suggestion from Udacity Project Rubric
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('section');//Selects all sections 
+    const navLink = document.querySelectorAll('nav a');//selects all 'a' link present inside nav
+  
+    function makeSectionActive() {
+      sections.forEach(section => {
+        const rect = section.getBoundingClientRect();//To detect the element location relative to the viewport 
+        const isInViewport = rect.top >= 0 && rect.bottom <= window.innerHeight;
+  
+        if (isInViewport){
+          const targetId = '#' + section.getAttribute('id');//If the section is in viewport the section Id gets appended to # to form the targetId
+          navLink.forEach(link => {
+            link.classList.remove('active');
+            if (link.getAttribute('href') === targetId) {
+              link.classList.add('active');//Setting the section active
+            }
+          });
+        }
+      });
+    }
+  
+    window.addEventListener('scroll', makeSectionActive);
+  });
+  
 
 
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
+// Scroll to anchor 
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();//To prevent the default behavior of the nav
+      const section = link.getAttribute('href');//To get the id of the section
+      const mainSection = document.querySelector(section);//To get the main section
+      mainSection.scrollIntoView({ behavior: 'smooth' });//Scrolling feature so that it scrolls to the section selected
+    });
+  });
 
